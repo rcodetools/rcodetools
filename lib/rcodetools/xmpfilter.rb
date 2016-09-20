8ac2a25c975d7377c10822b0159c43b3b5401340
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-09-20 18:33:31 +0900

    remove -w flag for interpreter to run xmpfilter

--- lib/rcodetools/xmpfilter.rb
+++ lib/rcodetools/xmpfilter.rb
@@ -27,7 +27,7 @@ class XMPFilter
                      :use_parentheses => true}
 
   Interpreter = Struct.new(:options, :execute_method, :accept_debug, :accept_include_paths, :chdir_proc)
-  INTERPRETER_RUBY = Interpreter.new(["-w"],
+  INTERPRETER_RUBY = Interpreter.new([],
     :execute_ruby, true, true, nil)
   INTERPRETER_RBTEST = Interpreter.new(["-S", File.join(File.dirname($0), "rbtest")],
     :execute_script, false, false, nil)
